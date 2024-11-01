629e757a362acb867942d445f59b0ea170883eba
SukanyaRani <srani6@asu.edu>
2024-11-01 14:48:17 -0700

    Movement of sword and both hand of player added

---
+++ Assets/Scripts/Controller/AnimatorHook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AnimatorHook : MonoBehaviour
+{
+    Animator anim;
+    StateManager states;
+    public void Init(StateManager st){
+        states=st;
+        anim=st.anim;
+    }
+    void onAnimatorMove(){
+        if(states.canMove)
+            return;
+
+        states.rigid.drag=0;
+        float multiplier=1;
+
+        Vector3 delta= anim.deltaPosition;
+        delta.y=0;
+
+        Vector3 v=(delta* multiplier)/states.delta;
+        states.rigid.velocity=v;
+    }
+
+}
--- Assets/Scripts/Controller/StateManager.cs
+++ Assets/Scripts/Controller/StateManager.cs
@@ -12,6 +12,8 @@ public class StateManager : MonoBehaviour
     public float horizontal;
     public float moveAmount;
     public Vector3 moveDir;
+    public bool rt, rb, lt, lb;
+    
 
     [Header("Stats")]
     public float moveSpeed = 2;
@@ -24,6 +26,9 @@ public class StateManager : MonoBehaviour
     public bool onGround;
     public bool run;
     public bool lockOn;
+    public bool inAction;
+    public bool canMove;
+    public bool isTwoHanded;
 
 
     [HideInInspector]
@@ -31,9 +36,14 @@ public class StateManager : MonoBehaviour
     [HideInInspector]
     public Rigidbody rigid;
     [HideInInspector]
+    public AnimatorHook a_hook;
+
+    [HideInInspector]
     public float delta;
     [HideInInspector]
     public LayerMask ignoreLayers;
+
+    float _actionDelay;
 
     public void Init()
     {
@@ -43,6 +53,9 @@ public class StateManager : MonoBehaviour
         rigid.angularDrag = 999;
         rigid.drag = 4;
         rigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        a_hook= activeModel.AddComponent<AnimatorHook>();
+        a_hook.Init(this);
 
         gameObject.layer = 8;
         ignoreLayers = ~(1 << 9);
@@ -75,6 +88,28 @@ public class StateManager : MonoBehaviour
     {
         delta = d;
 
+        DetectAction();
+
+        if(inAction){
+
+            anim.applyRootMotion=true;
+            _actionDelay+=delta;
+            if(_actionDelay>0.3f){
+                inAction=false;
+                _actionDelay=0;
+            }
+            else{
+            return;
+            }
+        }
+
+         canMove=anim.GetBool("canMove");
+
+         if(!canMove)
+            return;
+
+        anim.applyRootMotion=false;
+
         rigid.drag = (moveAmount > 0 || onGround == false) ? 0 : 4;
 
 
@@ -104,6 +139,39 @@ public class StateManager : MonoBehaviour
         HandleMovementAnimations();
     }
 
+    public void DetectAction(){
+
+        if(canMove==false)
+            return;
+
+        if(rt==false && rb==false && lt== false && lb==false)
+            return;
+        
+        string targetAnim=null;
+
+        if (rb)
+            targetAnim="oh_attack_1";
+
+        if (rt)
+            targetAnim="oh_attack_2";
+
+        if (lb)
+            targetAnim="oh_attack_3";
+
+        if (lt)
+            targetAnim="th_attack_1";
+
+        if(string.IsNullOrEmpty(targetAnim))
+            return;
+
+        canMove=false;
+        inAction=true;
+        
+        anim.CrossFade(targetAnim,0.2f);
+        //rigid.velocity=Vector3.zero;
+
+        
+    }
     public void Tick(float d)
     {
         delta = d;
@@ -131,5 +199,9 @@ public class StateManager : MonoBehaviour
             transform.position = targetPosition;
         }
         return r;
+    }
+
+    public void HandleTwoHanded(){
+        anim.SetBool("two_Handed", isTwoHanded);
     }
 }
